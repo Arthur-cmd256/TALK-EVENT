c918cf435dd006eb17a2b34ea40e21e87f09b65d
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2026-06-09 21:33:41 -0300

    Cria repositories

---
+++ src/main/java/com/talkevents/jpa/repositories/SessionRepository.java
@@ -0,0 +1,12 @@
+package com.talkevents.jpa.repositories;
+
+import com.talkevents.jpa.entities.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, UUID> {
+}
